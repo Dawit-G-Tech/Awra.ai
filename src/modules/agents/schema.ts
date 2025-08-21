822c9e561b86d8601167e82341f849614ccd7c4d
Dawit-G-Tech <dawit9514@gmail.com>
2025-08-21 11:13:31 +0300

    Agent Update + Delete procedure&UI

--- src/modules/agents/schema.ts
+++ src/modules/agents/schema.ts
@@ -3,3 +3,7 @@ export const agentsInsertschema = z.object({
     name: z.string().min(1,{ message: "Name is required"}),
     instructions: z.string().min(1,{ message: "Instructions are required"}),
 })
+
+export const agentsUpdateschema = agentsInsertschema.extend({
+    id: z.string().min(1,{ message:"Id is required"}),
+});
