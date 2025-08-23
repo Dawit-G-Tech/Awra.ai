2ca3cccda6e8cf528ee082226918578b838d4f1c
Dawit-G-Tech <dawit9514@gmail.com>
2025-08-23 14:34:05 +0300

    meetings_form + Dialog + commandselect & listheader

--- src/modules/agents/schema.ts
+++ src/modules/agents/schema.ts
@@ -2,7 +2,7 @@ import {z} from "zod";
 export const agentsInsertschema = z.object({
     name: z.string().min(1,{ message: "Name is required"}),
     instructions: z.string().min(1,{ message: "Instructions are required"}),
-})
+});
 
 export const agentsUpdateschema = agentsInsertschema.extend({
     id: z.string().min(1,{ message:"Id is required"}),
--- src/modules/meetings/server/procedure.ts
+++ src/modules/meetings/server/procedure.ts
@@ -5,8 +5,45 @@ import {z} from "zod";
 import { and, desc, eq, getTableColumns, ilike, count } from "drizzle-orm";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
 import { TRPCError } from "@trpc/server";
+import { meetingsInsertschema,meetingsUpdateschema } from "../schema";
 
 export const meetingsRouter = createTRPCRouter({
+    update: protectedProcedure
+            .input(meetingsUpdateschema)
+            .mutation(async ({ctx, input}) => {
+                const [updatedMeeting] = await db
+                    .update(meetings)
+                    .set(input)
+                    .where(
+                        and(
+                            eq(meetings.id, input.id),
+                            eq(meetings.userId, ctx.auth.user.id),
+                        )
+                    )
+                    .returning();
+    
+                if(!updatedMeeting) {
+                    throw new TRPCError({
+                        code: "NOT_FOUND",
+                        message: "Meeting not found",
+                    });
+                }
+    
+            return updatedMeeting;
+            }),
+
+    create:protectedProcedure.input(meetingsInsertschema).mutation(async({input,ctx}) =>{
+            const [createdMeeting] = await db
+            .insert(meetings)
+            .values({
+                ...input,
+                userId: ctx.auth.user.id,
+            })
+            .returning();
+            //todo: create streamcall , upsert stream users
+        return createdMeeting;
+        }),
+
     getOne: protectedProcedure.input(z.object({id: z.string()})).query(async({ input, ctx }) => {
         const [existingMeeting] = await db
             .select({
