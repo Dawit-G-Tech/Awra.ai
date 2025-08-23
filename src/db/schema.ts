fea92de8d32b460a4f1ab867f57bdc5bd76535be
Dawit-G-Tech <dawit9514@gmail.com>
2025-08-23 10:57:57 +0300

    meetings-setup-procedure-module &  pages

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -78,7 +78,7 @@ export const agents = pgTable("agents",{
   updatedAt: timestamp("updated_at").notNull().defaultNow(),
 });
 
-const meetingStatus = pgEnum("meeting_status", [
+export const meetingStatus = pgEnum("meeting_status", [
   "upcoming",
   "active",
   "completed",
